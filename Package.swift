fcf344e45ff0f98466ef2b2b7091d691a755800c
Uday Alla <ualla@g.ucla.edu>
2021-09-30 15:57:38 -0400

    Add SPM Support for Airship (#21)

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "mParticle-UrbanAirship",
+    platforms: [ .iOS(.v11) ],
+    products: [
+        .library(
+            name: "mParticle-UrbanAirship",
+            targets: ["mParticle-UrbanAirship"]),
+    ],
+    dependencies: [
+      .package(name: "mParticle-Apple-SDK",
+               url: "https://github.com/mParticle/mparticle-apple-sdk",
+               .upToNextMajor(from: "8.0.0")),
+      .package(name: "Airship",
+               url: "https://github.com/urbanairship/ios-library",
+               .upToNextMajor(from: "15.0.0")),
+    ],
+    targets: [
+        .target(
+            name: "mParticle-UrbanAirship",
+            dependencies: [
+                .byName(name: "mParticle-Apple-SDK"),
+                .product(name: "AirshipCore", package: "Airship"),
+            ],
+            path: "mParticle-UrbanAirship",
+            publicHeadersPath: "."),
+    ]
+)
