2b70957d773ede696de95d5bca73192614a6821e
Ben Baron <bbaron@mparticle.com>
2023-12-06 10:25:10 -0500

    test

--- mParticle-UrbanAirship/MPKitUrbanAirship.h
+++ mParticle-UrbanAirship/MPKitUrbanAirship.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
-#if defined(__has_include) && __has_include(<mParticle_Apple_SDK/mParticle-Apple-SDK-umbrella.h>)
+#if defined(__has_include) && __has_include(<mParticle_Apple_SDK/mParticle-Apple-SDK.h>)
     #import <mParticle_Apple_SDK/mParticle-Apple-SDK.h>
-#elif defined(__has_include) && __has_include(<mParticle_Apple_SDK_NoLocation/mParticle-Apple-SDK-umbrella.h>)
+#elif defined(__has_include) && __has_include(<mParticle_Apple_SDK_NoLocation/mParticle-Apple-SDK.h>)
     #import <mParticle_Apple_SDK_NoLocation/mParticle-Apple-SDK.h>
 #else
     #import "mParticle-Apple-SDK.h"
